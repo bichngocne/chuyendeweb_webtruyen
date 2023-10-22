6faaa4a3294b351c65a49e92a65004aff6223ec2
ngocty <nb447105@gmail.com>
2023-10-22 12:24:29 +0700

    update main

--- server/server.js
+++ server/server.js
@@ -1,26 +1,30 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import express from "express";
+import {createProxyMiddleware} from "http-proxy-middleware";
+
 dotenv.config();
-//import
-import http from 'http';
+const app = express();
 
-// Creating the node server
-const SERVER = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-  });
 
-// GET port
+// Cấu hình proxy middleware
+var options = {
+  target: "http://localhost:5000", // Địa chỉ máy chủ hoặc API bạn muốn proxy đến
+  changeOrigin: true, // Cần thiết để proxy cho các trang web có tên miền ảo
+  ws: true, // Proxy cho websockets
+  pathRewrite: {
+    "^/api": "/api"
+    }
+};
+// Tạo proxy (mà không cần context)
+var exampleProxy = createProxyMiddleware(options);
+app.use("/api", exampleProxy);
+
+// Định nghĩa cổng và khởi động server
 const PORT = process.env.PORT || 5000;
-
-// Firing up the server on selected port
-SERVER.listen(PORT);
-
-SERVER.on("listening", () => {
-    console.log("[Server]::LISTEN:%s", PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
-// Callback function for checking connecting or error
-SERVER.on("error", error => {
-    throw new Error(`[Server]::ERROR:${error.message}`);
+app.on("error", (error) => {
+  console.error(`Server error: ${error.message}`);
 });
