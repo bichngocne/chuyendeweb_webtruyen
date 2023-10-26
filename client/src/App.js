8de397b3db851633824104a31412634bd2870f14
ngocty <nb447105@gmail.com>
2023-10-26 08:29:07 +0700

    last change main

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
 // import { useSelector,useDispatch } from "react-redux";
-import { Home, PostStory,ListStory,Category,TrashcanStory } from "./container/system/poster";
+import {
+  PostStory,
+  ListStory,
+  Category,
+  TrashcanStory,
+} from "./container/system/poster";
 import { Public } from "./container/public";
 import { Route, Routes } from "react-router-dom";
 import path from "./ultis/path";
+import React, { Suspense } from "react";
+const HomePoster = React.lazy(() => import("./container/system/poster/Home"));
+
 function App() {
   return (
     <>
       <div className="">
-        <Routes>
-          {/* Tuyến đường của poster */}
-        <Route path={path.HOME_FOR_POSTER} element={<Home />}>
-            <Route path={path.POST_STORY} element={<PostStory />}/>
-            <Route path={path.LIST_STORY} element={<ListStory />}/>
-            <Route path={path.CHO_DUYET} element={<PostStory />}/>
-            <Route path={path.CATEGORY_STORY_POSTER} element={<Category />}/>
-            <Route path={path.STORY_NEED_UPDATE} element={<PostStory />}/>
-            <Route path={path.THUNG_RAC} element={<TrashcanStory />}/>
-          </Route>
-        </Routes>
+        <Suspense>
+          <Routes>
+            {/* Tuyến đường của poster */}
+            <Route path={path.HOME_FOR_POSTER} element={<HomePoster />}>
+              <Route path={path.POST_STORY} element={<PostStory />} />
+              <Route path={path.LIST_STORY} element={<ListStory />} />
+              <Route path={path.CHO_DUYET} element={<PostStory />} />
+              <Route path={path.CATEGORY_STORY_POSTER} element={<Category />} />
+              <Route path={path.STORY_NEED_UPDATE} element={<PostStory />} />
+              <Route path={path.THUNG_RAC} element={<TrashcanStory />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </div>
     </>
   );
