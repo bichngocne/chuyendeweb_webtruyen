7b61a5a08cd4dd1ba752410f93ea35ee9e3c6c22
ngocty <nb447105@gmail.com>
2023-11-11 09:35:25 +0700

    dev

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 // import { useSelector,useDispatch } from "react-redux";
-import {
-  PostStory,
-  ListStory,
-  Category,
-  TrashcanStory,
-} from "./container/system/poster";
+import { Home, PostStory,ListStory,Category,TrashcanStory, Story, Chapper, UpdateStory, PendingStory } from "./container/system/poster";
 import { Public } from "./container/public";
+
 import { Route, Routes } from "react-router-dom";
 import path from "./ultis/path";
+
 import React, { Suspense } from "react";
 const HomePoster = React.lazy(() => import("./container/system/poster/Home"));
+const HomeReader = React.lazy(() => import("./container/system/reader/Home"));
 
 function App() {
   return (
@@ -20,11 +18,21 @@ function App() {
             {/* Tuyến đường của poster */}
             <Route path={path.HOME_FOR_POSTER} element={<HomePoster />}>
               <Route path={path.POST_STORY} element={<PostStory />} />
+              <Route
+                path={path.STORY__NAME__ID_UPDATE}
+                element={<PostStory />}
+              />
               <Route path={path.LIST_STORY} element={<ListStory />} />
-              <Route path={path.CHO_DUYET} element={<PostStory />} />
+              <Route path={path.CHO_DUYET} element={<PendingStory />} />
               <Route path={path.CATEGORY_STORY_POSTER} element={<Category />} />
-              <Route path={path.STORY_NEED_UPDATE} element={<PostStory />} />
+              <Route path={path.STORY_NEED_UPDATE} element={<UpdateStory />} />
               <Route path={path.THUNG_RAC} element={<TrashcanStory />} />
+              <Route path={path.STORY__NAME__ID} element={<Story />} />
+              <Route path={path.CHAPPER__NAME__ID} element={<Chapper />} />
+            </Route>
+            {/* Tuyến đường của reader */}
+            <Route>
+              <Route path={path.HOME_FOR_READER} element={<HomeReader />} />
             </Route>
           </Routes>
         </Suspense>
