6543af7b7ed47d16bd02deb39795e02768cdf1d4
ngocty <nb447105@gmail.com>
2023-11-23 11:13:30 +0700

    change drop category

--- client/src/components/poster/DropdownCategory.js
+++ client/src/components/poster/DropdownCategory.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as actions from "../../store/actions";
 import { encryptData } from "../../ultis/function";
-import * as apis from '../../apis'
 const DropdownCategory = ({ value, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedValue, setSelectedValue] = useState([]);
@@ -12,23 +11,27 @@ const DropdownCategory = ({ value, onChange }) => {
 
   const dispatch = useDispatch();
   const [rowsWithSTT, setRowsWithSTT] = useState([]);
+  const { data } = useSelector((state) => state.app);
   const [text, setText] = useState("");
   //get categories
   useEffect(() => {
-    const fetchCategory = async () => {
-      const response = await apis.getAllCategories();
-      console.log(response);
-      if (response.status === 200) {
-        setRowsWithSTT(response.data.categories);
-      }
-    };
-    fetchCategory();
+    dispatch(actions.getCategories());
   }, []);
+  useEffect(() => {
+    if (data) {
+      const updatedData = data.map((row, index) => ({
+        ...row,
+        STT: index + 1,
+      }));
+      setRowsWithSTT(updatedData);
+    }
+  }, [data]);
+  console.log(rowsWithSTT);
   // create on change when search
   const handleChange = (value) => {
     setText(value);
     setRowsWithSTT(
-      rowsWithSTT.filter((item) => {
+      data.filter((item) => {
         return Object.keys(item).some((key) => {
           if (typeof item[key] === 'string') {
             return item[key].toLowerCase().includes(value.toLowerCase());
